417698cbba2015e82dee475b437b0b062795f3fa
Luca Maltagliati <luca.maltagliati@mia-platform.eu>
2022-04-03 19:59:50 +0200

    feat: progress in exercises

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,13 +1,11 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(10);
 }
 
-fn call_me(num: u32) {
+fn call_me(num: u64) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
