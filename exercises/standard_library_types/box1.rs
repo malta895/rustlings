074697abce2985adfa27abe6d45389b4604fa25c
Luca Maltagliati <luca.maltagliati@mia-platform.eu>
2022-04-10 23:01:17 +0200

    some other solved exercises

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,24 +16,25 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
-use std::clone::Clone;
-
 #[derive(PartialEq, Debug)]
-pub enum List<T: Clone + Copy> {
+pub enum List<T> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-impl<T: Clone + Copy> List<T> {
-    pub fn from_vec(input_vec: Vec<T>) -> List<T> {
-        if input_vec.is_empty() {
+// the next implementation are valid only on T that have Copy trait, hence we have to declare it
+impl<T: Copy> List<T> {
+    pub fn from_vec(input_vec: &Vec<T>) -> List<T> {
+        List::from_slice(&input_vec[..])
+    }
+
+    pub fn from_slice(input_slice: &[T]) -> List<T> {
+        if input_slice.is_empty() {
             List::Nil
         } else {
             List::Cons(
-                input_vec[0],
-                Box::new(List::from_vec((&input_vec[1..]).to_vec())),
+                input_slice[0],
+                Box::new(List::from_slice(&input_slice[1..])),
             )
         }
     }
@@ -72,18 +73,36 @@ mod tests {
     #[test]
     fn test_create_empty_list_from_empty_vec() {
         let input_vec: Vec<i32> = vec![];
-        assert_eq!(List::Nil, List::from_vec(input_vec),)
+        assert_eq!(List::Nil, List::from_vec(&input_vec))
     }
 
     #[test]
     fn test_create_list_from_vec() {
         let input_vec = vec![1, 2, 3];
         assert_eq!(
+            List::from_vec(&input_vec),
             List::Cons(
                 1,
                 Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
             ),
-            List::from_vec(input_vec),
+        )
+    }
+
+    #[test]
+    fn test_create_list_from_vec_with_strings() {
+        let input_vec = vec!["1", "2", "3", "ciao"];
+        assert_eq!(
+            List::from_vec(&input_vec),
+            List::Cons(
+                "1",
+                Box::new(List::Cons(
+                    "2",
+                    Box::new(List::Cons(
+                        "3",
+                        Box::new(List::Cons("ciao", Box::new(List::Nil)))
+                    ))
+                )),
+            ),
         )
     }
 }
